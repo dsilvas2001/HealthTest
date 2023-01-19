a61a516b3c27f722a021e4dab25af59f6bb4c53f
dsilvas2001 <dsilvas@uteq.edu.ec>
2023-01-19 08:30:44 -0500

    V1.8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PerfiluserComponent } from './componentes/perfiluser/perfiluser.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PerfiluserComponent
+    PerfiluserComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
