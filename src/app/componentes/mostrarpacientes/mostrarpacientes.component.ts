ba435d44c1b1f18e59948c665cb846f6b182bf8f
dsilvas2001 <dsilvas@uteq.edu.ec>
2023-02-18 10:18:12 -0500

    tipouser

--- src/app/componentes/mostrarpacientes/mostrarpacientes.component.ts
+++ src/app/componentes/mostrarpacientes/mostrarpacientes.component.ts
@@ -1,25 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CargarScriptsService } from 'src/app/cargar-scripts.service';
-
-
-
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Persona } from 'src/app/Modelo/Persona';
+import { Usuario } from 'src/app/Modelo/Usuario';
+import { Router } from '@angular/router';
+import { ServiceService } from 'src/app/Services/service.service';
 
 @Component({
   selector: 'app-mostrarpacientes',
   templateUrl: './mostrarpacientes.component.html',
   styleUrls: ['./mostrarpacientes.component.css']
 })
-export class MostrarpacientesComponent {
+export class MostrarpacientesComponent implements OnInit{
+  model: any = {};
+  getData: boolean;
+  personas: Persona = new Persona();
 
-  constructor(private _CargarScripts: CargarScriptsService){
+
+  constructor(
+    private _CargarScripts: CargarScriptsService,
+    public formulario: FormBuilder,
+    private router: Router,
+    private service: ServiceService
+  ) {
     _CargarScripts.Pagadmin(["scriptpagadmin"]);
     _CargarScripts.Webfont();
 
     this.scrippagprincipal();
-
   }
+
   ngOnInit(){
+    console.log('initializing');
+
     this.cargarIcons();
+    this.service.getPersonas().subscribe((data) => {
+      console.log('Objecto persona: ', this.personas);
+
+      this.personas = data;
+    });
 
 }
 cargarIcons(){
--- src/assets/Js/selectipouser.js
+++ src/assets/Js/selectipouser.js
@@ -1,6 +1,10 @@
 const select = document.querySelector('#select');
 const opciones = document.querySelector('#opciones');
+const select1 = document.querySelector('#select1');
+const opciones1 = document.querySelector('#opciones1');
 const contenidoSelect = document.querySelector('#select .contenido-select');
+const contenidoSelect1 = document.querySelector('#select1 .contenido-select');
+
 const hiddenInput = document.querySelector('#inputSelect');
 
 document.querySelectorAll('#opciones > .opcion').forEach((opcion) => {
@@ -12,8 +16,22 @@ document.querySelectorAll('#opciones > .opcion').forEach((opcion) => {
 		hiddenInput.value = e.currentTarget.querySelector('.titulo').innerText;
 	});
 });
+document.querySelectorAll('#opciones1 > .opcion').forEach((opcion) => {
+	opcion.addEventListener('click', (e) => {
+		e.preventDefault();
+		contenidoSelect1.innerHTML = e.currentTarget.innerHTML;
+		select1.classList.toggle('active');
+		opciones1.classList.toggle('active');
+		hiddenInput.value = e.currentTarget.querySelector('.titulo').innerText;
+	});
+});
 
 select.addEventListener('click', () => {
 	select.classList.toggle('active');
 	opciones.classList.toggle('active');
+});
+
+select1.addEventListener('click', () => {
+	select1.classList.toggle('active');
+	opciones1.classList.toggle('active');
 });
